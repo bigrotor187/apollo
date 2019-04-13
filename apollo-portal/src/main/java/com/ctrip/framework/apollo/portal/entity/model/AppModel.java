92756c129e1611b2bd26e283b110e0cd80477d75
bigrotor187 <bigrotor187@gmail.com>
2019-04-13 22:05:25 +0800

    Add comments.

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/entity/model/AppModel.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/entity/model/AppModel.java
@@ -6,27 +6,43 @@ import java.util.Set;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+/**
+ * App Model
+ */
 public class AppModel {
 
+  /**
+   * App 名
+   */
   @NotBlank(message = "name cannot be blank")
   private String name;
-
+  /**
+   * App 编号
+   */
   @NotBlank(message = "appId cannot be blank")
   @Pattern(
       regexp = InputValidator.CLUSTER_NAMESPACE_VALIDATOR,
       message = "AppId格式错误: " + InputValidator.INVALID_CLUSTER_NAMESPACE_MESSAGE
   )
   private String appId;
-
+  /**
+   * 部门编号
+   */
   @NotBlank(message = "orgId cannot be blank")
   private String orgId;
-
+  /**
+   * 部门名称
+   */
   @NotBlank(message = "orgName cannot be blank")
   private String orgName;
-
+  /**
+   * 拥有人姓名
+   */
   @NotBlank(message = "ownerName cannot be blank")
   private String ownerName;
-
+  /**
+   * 管理员编号集合
+   */
   private Set<String> admins;
 
   public String getName() {
